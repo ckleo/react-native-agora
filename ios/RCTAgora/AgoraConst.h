bdb3006c00e983d6db143be4c25b4cdcc25d2e9d
Huanchen Zhang <huanchen1126@gmail.com>
2017-09-22 22:33:18 +0800

    update agora sdk

--- ios/RCTAgora/AgoraConst.h
+++ ios/RCTAgora/AgoraConst.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017年 Syan. All rights reserved.
 //
 
-#import <AgoraRtcEngineKit/AgoraRtcEngineKit.h>
+#import <AgoraAudioKit/AgoraRtcEngineKit.h>
 
 @interface AgoraConst : NSObject
 
--- ios/RCTAgora/RCTAgora.h
+++ ios/RCTAgora/RCTAgora.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <React/RCTBridgeModule.h>
-#import <AgoraRtcEngineKit/AgoraRtcEngineKit.h>
+#import <AgoraAudioKit/AgoraRtcEngineKit.h>
 
 @interface RCTAgora : NSObject<RCTBridgeModule, AgoraRtcEngineDelegate>
 
